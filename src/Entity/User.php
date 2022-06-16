b8d575473956135fa59fc533fc693a3d5cf27b34
Kayo Bruno <kayodw@gmail.com>
2022-06-16 13:15:21 -0300

    Add Entity structure

---
+++ src/Entity/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Entity;
+
+use App\Contract\Entity;
+
+class User implements Entity
+{
+    public function __construct(
+        private readonly int $id,
+        private readonly string $name,
+        private readonly string $email
+    ) { }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+}
